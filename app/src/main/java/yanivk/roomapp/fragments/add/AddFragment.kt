04226485c25cea2c29c8540869b3b34aa65fd212
Yaniv Kremer <yanivkr7@gmail.com>
2022-05-08 06:25:30 +0300

    *

---
+++ app/src/main/java/yanivk/roomapp/fragments/add/AddFragment.kt
@@ -0,0 +1,20 @@
+package yanivk.roomapp.fragments.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import yanivk.roomapp.R
+
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
